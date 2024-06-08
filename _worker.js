dd02cf2dd5d20d74acb65fa3addd40dcde7e7743
ifSadEveryDay <32483950+ifSadEveryDay@users.noreply.github.com>
2024-06-08 19:29:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -73,7 +73,7 @@ async function nginx() {
 	<!DOCTYPE html>
 	<html>
 	<head>
-	<title>Welcome to nginx!</title>
+	<title>Welcome!</title>
 	<style>
 		body {
 			width: 35em;
@@ -83,16 +83,7 @@ async function nginx() {
 	</style>
 	</head>
 	<body>
-	<h1>Welcome to nginx!</h1>
-	<p>If you see this page, the nginx web server is successfully installed and
-	working. Further configuration is required.</p>
-	
-	<p>For online documentation and support please refer to
-	<a href="http://nginx.org/">nginx.org</a>.<br/>
-	Commercial support is available at
-	<a href="http://nginx.com/">nginx.com</a>.</p>
-	
-	<p><em>Thank you for using nginx.</em></p>
+	<h1>Welcome!</h1>
 	</body>
 	</html>
 	`
